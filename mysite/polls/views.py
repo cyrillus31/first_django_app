48422e5cd209d0362e8300ffd6f6d471406760dc
Kirill F <kirill.olegovich31@gmail.com>
2024-01-07 00:36:39 +0300

    j

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 
@@ -13,11 +13,15 @@ def index(request):
     context = {
             "latest_question_list": latest_question_list,
             }
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    context = {"question": question}
+    return render(request, "polls/detail.html", context)
+
 
 
 def results(request, question_id):
