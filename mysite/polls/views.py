958719a61e011aef415249caae23a04f6375173d
Kirill F <kirill.olegovich31@gmail.com>
2024-01-06 23:59:30 +0300

    adding first tremplate

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+
+from .models import Question
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello! You are at the polls index page!")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+            "latest_question_list": latest_question_list,
+            }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    response = f"You are looking at the results of question {question_id}"
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse(f"You're votin on question {question_id}")
+
 
 
 
